404236f43d762473dc714e6dfb70eeee3f904db7
hala daher <haladaher5555@gmail.com>
2025-02-10 00:00:29 +0200

    added forgot password page

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,16 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HomePage from "./pages/homepage";
 import Login from "./pages/loginpage"
+import ForgotPsswordPage from './pages/forgotpasswordpage'
+
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/loginpage" element={<Login />} />
+        <Route path="/forgot-password" element={<ForgotPsswordPage />} />
+      
       </Routes>
     </Router>
   );
