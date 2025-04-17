b30784a1b21047a2f45cba056d69e99e2d703f11
Lea Mourad <leamurad20@gmail.com>
2025-04-17 10:09:21 +0300

    added urls change email

--- authenticate/urls.py
+++ authenticate/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import UserRegistrationView, EmailVerificationView, LoginView, LogoutView, AdminLoginView
+from .views import UserRegistrationView, EmailVerificationView, LoginView, LogoutView, AdminLoginView,ChangeEmailView,VerifyEmailChangeView,CurrentUserView,DeleteAccountView
 
 urlpatterns = [
     path('register/', UserRegistrationView.as_view(), name='register'),
@@ -7,4 +7,8 @@ urlpatterns = [
     path('login/', LoginView.as_view(), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
     path('admin-login/', AdminLoginView.as_view(), name='admin-login'),
+    path('change-email/', ChangeEmailView.as_view(), name='change-email'),   
+    path('verify-email-change/', VerifyEmailChangeView.as_view(),name='verify-email-change'),
+    path('delete-account/', DeleteAccountView.as_view(), name='delete-account'),
+    path('me/', CurrentUserView.as_view(), name='current-user'),
 ]
