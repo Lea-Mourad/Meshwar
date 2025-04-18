e8d0526b948bd9dc21cc24f825f293dd7576c053
Lea Mourad <leamurad20@gmail.com>
2025-04-18 11:58:17 +0300

    removed search engine frontend

--- Meshwar-frontend/src/pages/homepage.jsx
+++ Meshwar-frontend/src/pages/homepage.jsx
@@ -42,21 +42,7 @@ const HomePage = () => {
             Sign Up
           </Link>
         </div>
-        </div>
-        {/* Search Bar */}
-        <div className="flex flex-col items-center justify-center py-40 bg-gray-100">
-        <h3 className="text-3xl font-semibold text-gray-800 mb-6">Search for Places</h3>
-        
-        <div className="flex items-center w-full max-w-3xl border border-gray-300 rounded-full p-3 shadow-lg bg-white">
-          <input 
-            type="text" 
-            placeholder="Search for places..." 
-            className="w-full px-6 py-4 text-lg border-none focus:outline-none rounded-full"
-          />
-          <button className="px-8 py-4 bg-[#984949] text-white text-lg font-bold rounded-full hover:bg-[#7c3b3b] transition duration-300">
-            Search
-          </button>
-          </div>
+  
       </div>
        {/* Currency Converter */}
        <div className="flex flex-col items-center justify-center py-20 bg-white">
