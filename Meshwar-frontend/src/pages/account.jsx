1f515b92dab17be3f279e16cf0d66fb71ebfe0f5
Lea Mourad <leamurad20@gmail.com>
2025-04-19 14:47:04 +0300

    removed delete account account.jsx

--- Meshwar-frontend/src/pages/account.jsx
+++ Meshwar-frontend/src/pages/account.jsx
@@ -154,7 +154,7 @@ const Accountpage = () => {
       navigate("/loginpage");
     }
   };
-
+/*
   const handleDeleteAccount = async () => {
     const token = localStorage.getItem("authToken");
 
@@ -179,7 +179,7 @@ const Accountpage = () => {
       }
     }
   };
-
+*/
   return (
     <div className="relative min-h-screen bg-[url('https://www.lebanontours.co/uploads/1/0/3/7/10373098/arches-pigeon-rocks-beirut_orig.jpg')] bg-cover bg-center">
       <Header />
@@ -281,7 +281,7 @@ const Accountpage = () => {
               </form>
             </>
           )}
-
+{/*
           <div className="mt-8">
             <button
               className="py-4 px-8 bg-gray-600 text-white rounded-md hover:bg-gray-700 transition text-sm"
@@ -290,10 +290,12 @@ const Accountpage = () => {
               Delete Account
             </button>
           </div>
+          */}
+          
         </div>
       </div>
 
-      {/* Logout Confirmation Popup */}
+      { /*Logout Confirmation Popup */}
       {showLogoutPopup && (
         <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center">
           <div className="bg-white p-6 rounded-lg shadow-lg w-96">
@@ -316,7 +318,7 @@ const Accountpage = () => {
         </div>
       )}
 
-      {/* Delete Account Popup */}
+      {/* Delete Account Popup }
       {showDeletePopup && (
         <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center">
           <div className="bg-white p-6 rounded-lg shadow-lg w-96">
@@ -341,6 +343,7 @@ const Accountpage = () => {
           </div>
         </div>
       )}
+      */}
     </div>
   );
 };
