7ff81b91b40fd4ef22cc7cfbc83e0bd7301207c4
Nadjoubiii <146746684+Nadjoubiii@users.noreply.github.com>
2025-04-22 08:58:38 +0300

    Update serializers.py

--- authenticate/serializers.py
+++ authenticate/serializers.py
@@ -19,6 +19,11 @@ logger = logging.getLogger(__name__)
 # Initialize Postmark client
 postmark = PostmarkClient(server_token=settings.POSTMARK['TOKEN'])
 
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'email', 'is_verified', 'is_staff', 'is_active']
+
 class UserRegistrationSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True)
     is_staff = serializers.BooleanField(required=False, default=False)
