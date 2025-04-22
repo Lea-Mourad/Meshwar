cf88ada3c06b56ecda728f085acffb963569ea89
Nadjoubiii <146746684+Nadjoubiii@users.noreply.github.com>
2025-04-22 08:57:56 +0300

    Update models.py

--- authenticate/models.py
+++ authenticate/models.py
@@ -21,20 +21,18 @@ class UserManager(BaseUserManager):
         extra_fields.setdefault('is_staff', True)
         extra_fields.setdefault('is_superuser', True)
 
-        if extra_fields.get('is_staff') is not True:
-            raise ValueError('Superuser must have is_staff=True.')
-        if extra_fields.get('is_superuser') is not True:
-            raise ValueError('Superuser must have is_superuser=True.')
-
+        if not extra_fields.get('is_staff') or not extra_fields.get('is_superuser'):
+            raise ValueError('Superuser must have is_staff=True and is_superuser=True.')
+        
         return self.create_user(email, password, **extra_fields)
 
 class User(AbstractUser):
-    username = None  # Remove the username field
+    username = None
     email = models.EmailField(unique=True)
     is_verified = models.BooleanField(default=False)
 
-    USERNAME_FIELD = 'email'  # Use email as the username field
-    REQUIRED_FIELDS = []  # No additional required fields
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
 
     objects = UserManager()
 
